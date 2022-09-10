51b580a5fd5c0552d747e30321d28d9f46ed1f94
Trey <39141616+curv3ball@users.noreply.github.com>
2022-09-10 19:10:11 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-from ast import PyCF_ONLY_AST
 import os
 import win32api
 import win32con
@@ -9,8 +8,6 @@ import keyboard
 import multiprocessing
 import pyautogui
 import images
-import regions
-
 from discord_webhook import *
 
 webhook_url = ""
@@ -23,7 +20,6 @@ webhook = DiscordWebhook(url=webhook_url, rate_limit_retry = True)
 default_confidence = 0.9
 activeResponse = None
 safeSleep = 2 - (0.025 * multiprocessing.cpu_count())
-lastUpdate = 0
 
 def currentTime():
     return (time.strftime("%I:%M %p"))
@@ -40,37 +36,29 @@ def mouseClick(msg, a):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
     time.sleep(safeSleep)
 
-def locateCenter(image, region, x = 0.9):
-    return pyautogui.locateCenterOnScreen(image, region = (region), confidence = x)
-
-def locate(image, region, x = 0.9):
-    return pyautogui.locateOnScreen(image, region = (region), confidence = x)
-
 def play():
     confidence = default_confidence
     
-    searching = locateCenter(images.searching, regions.searching, confidence)
+    searching = pyautogui.locateCenterOnScreen(images.searching, (1045, 115, 70, 50), confidence)
 
     if searching != None:
         return
 
-    try: champions_button = locateCenter(images.champions, regions.main_menu, confidence)
-    except: print("failed to run locateCenter() on champions_button")
+    champions_button = pyautogui.locateCenterOnScreen(images.champions, (65, 175, 500, 350), confidence)
         
-    try: play_button = locateCenter(images.play_button, regions.main_menu, confidence)
-    except: print("failed to run locateCenter() on play_button")
+    play_button = pyautogui.locateCenterOnScreen(images.play_button, (65, 175, 500, 350), confidence)
 
     if champions_button != None and play_button != None:
-        try: mouseClick("champions_button", champions_button)
-        except: print("mouseClick failed [location= " + str(champions_button) + "]")
+        print("clicking champions_button")
+        mouseClick("champions_button", champions_button)
 
-        try: evie = locate(images.evieSmall, regions.champions, 0.7)
-        except: print("failed to run locate() on evie")
+        evie = pyautogui.locateOnScreen(images.evieSmall, (65, 240, 1000, 625), 0.7)
 
         if evie != None:
+            print("sending discord webhook")
             championSnapshot = pyautogui.screenshot(region=(evie[0] - 5, evie[1] - 3, 100, 100))
             championSnapshot.save(images.currentLevel)
-            profileSnapshot = pyautogui.screenshot(region=(regions.player_profile))
+            profileSnapshot = pyautogui.screenshot(region=(1460, 963, 413, 93))
             profileSnapshot.save(images.currentProfile)
 
             webhook = DiscordWebhook(url = webhook_url, rate_limit_retry = True)
@@ -95,45 +83,8 @@ def play():
 
         time.sleep(safeSleep)
 
-        try: play_button = locateCenter(images.play_button, regions.main_menu, confidence)
-        except: print("failed to run locateCenter() on play_button")
+        play_button = pyautogui.locateCenterOnScreen(images.play_button, (65, 175, 500, 350), confidence)
 
         if play_button != None:
-            try: mouseClick("play_button", play_button)
-            except: print("mouseClick failed [location= " + str(play_button) + "]")
-
-def startMatch():
-    confidence = default_confidence
-    if not keyboard.is_pressed("delete"):
-        gamemode_top = None
-        gamemode_bottom = None
-        
-        try: gamemode_top = locateCenter(images.training_gamemode, regions.gamemode_select_top, confidence)
-        except: print("failed to run locateCenter() on gamemode_top")
-
-        if gamemode_top != None:
-            try: mouseClick("gamemode_top", gamemode_top)
-            except: print("mouseClick failed [location= " + str(gamemode_top) + "]")
-
-        try: gamemode_bottom = locateCenter(images.tdm_training, regions.gamemode_select_bottom, confidence)
-        except: print("failed to run locateCenter() on gamemode_bottom")
-        
-        if gamemode_bottom != None:
-            try: mouseClick("gamemode_bottom", gamemode_bottom)
-            except: print("mouseClick failed [location= " + str(gamemode_bottom) + "]")
-
-def main():
-    print("______       _            _ _           ")
-    print("| ___ \     | |          | (_)          ")
-    print("| |_/ / ___ | |_ __ _  __| |_ _ __  ___ ")
-    print("| ___ \/ _ \| __/ _` |/ _` | | '_ \/ __|")
-    print("| |_/ / (_) | || (_| | (_| | | | | \__ " + '\\')
-    print("\____/ \___/ \__\__,_|\__,_|_|_| |_|___/  v2.8.5 by curv3#0984\n\n")
-
-    confidence = default_confidence                    
-    
-    while not keyboard.is_pressed("delete"):
-        play()
-
-main()
-sys.exit(0)
+            print("clicking play_button")
+            mouseClick("play_button", play_button)
