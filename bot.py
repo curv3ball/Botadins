0c801ff75fad849b5cccf080799f4eb9cfae251f
Trey <39141616+curv3ball@users.noreply.github.com>
2022-09-10 18:53:46 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,19 +6,112 @@ import ctypes
 import sys
 import functions
 import keyboard
+import multiprocessing
+import pyautogui
+import images
+import regions
 
-print("______       _            _ _           ")
-print("| ___ \     | |          | (_)          ")
-print("| |_/ / ___ | |_ __ _  __| |_ _ __  ___ ")
-print("| ___ \/ _ \| __/ _` |/ _` | | '_ \/ __|")
-print("| |_/ / (_) | || (_| | (_| | | | | \__ " + '\\')
-print("\____/ \___/ \__\__,_|\__,_|_|_| |_|___/  v2.8.5 by curv3#0984\n\n")
-                                    
-while not keyboard.is_pressed("delete"):
-    functions.play()
-    functions.startMatch()
-    functions.lockChampion()
-    functions.spawnChampion()
-    functions.antiAFK()
+from discord_webhook import *
+
+webhook_url = ""
+
+file = open(os.path.dirname(__file__) + '/../webhook.txt')
+for line in file:
+    webhook_url = line
+
+webhook = DiscordWebhook(url=webhook_url, rate_limit_retry = True)
+default_confidence = 0.9
+activeResponse = None
+safeSleep = 2 - (0.025 * multiprocessing.cpu_count())
+lastUpdate = 0
+
+def currentTime():
+    return (time.strftime("%I:%M %p"))
+
+def mouseMove(x, y):
+    win32api.SetCursorPos((x, y))
+
+def mouseClick(msg, a):
+    print("clicking " + msg + " @ " + str(a))
+    mouseMove(a[0], a[1])
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    time.sleep(safeSleep)
+
+def locateCenter(image, region, x = 0.9):
+    return pyautogui.locateCenterOnScreen(image, region = (region), confidence = x)
+
+def locate(image, region, x = 0.9):
+    return pyautogui.locateOnScreen(image, region = (region), confidence = x)
+
+def main():
+    print("______       _            _ _           ")
+    print("| ___ \     | |          | (_)          ")
+    print("| |_/ / ___ | |_ __ _  __| |_ _ __  ___ ")
+    print("| ___ \/ _ \| __/ _` |/ _` | | '_ \/ __|")
+    print("| |_/ / (_) | || (_| | (_| | | | | \__ " + '\\')
+    print("\____/ \___/ \__\__,_|\__,_|_|_| |_|___/  v2.8.5 by curv3#0984\n\n")
+    confidence = default_confidence                    
+    while not keyboard.is_pressed("delete"):
+        searching = None
+        try: searching = locateCenter(images.searching, regions.searching, confidence)
+        except: print("failed to run locateCenter() on searching")
+
+        if searching != None:
+            return
+        
+        champions_button = None
+        play_button = None
+
+        try: champions_button = locateCenter(images.champions, regions.main_menu, confidence)
+        except: print("failed to run locateCenter() on champions_button")
+        
+        try: play_button = locateCenter(images.play_button, regions.main_menu, confidence)
+        except: print("failed to run locateCenter() on play_button")
+
+        if champions_button != None and play_button != None:
+            try: mouseClick("champions_button", champions_button)
+            except: print("mouseClick failed [location= " + str(champions_button) + "]")
+
+            try: evie = locate(images.evieSmall, regions.champions, 0.7)
+            except: print("failed to run locate() on evie")
+
+            if evie != None:
+                championSnapshot = pyautogui.screenshot(region=(evie[0] - 5, evie[1] - 3, 100, 100))
+                championSnapshot.save(images.currentLevel)
+                profileSnapshot = pyautogui.screenshot(region=(regions.player_profile))
+                profileSnapshot.save(images.currentProfile)
+
+                webhook = DiscordWebhook(url = webhook_url, rate_limit_retry = True)
+                webhook.remove_file('attachment://file1.png')
+                webhook.remove_file('attachment://file2.png')
+
+                with open(images.currentLevel, "rb") as f:
+                    webhook.add_file(file=f.read(), filename="file1.png")
+
+                with open(images.currentProfile, "rb") as b:
+                    webhook.add_file(file=b.read(), filename="file2.png")
+
+                global activeResponse
+
+                if activeResponse != None:
+                    webhook.delete(activeResponse)
+
+                activeResponse = webhook.execute()
+
+            pyautogui.keyDown('escape'); time.sleep(0.1)
+            pyautogui.keyUp('escape'); time.sleep(1)
+
+            time.sleep(safeSleep)
+
+            try: play_button = locateCenter(images.play_button, regions.main_menu, confidence)
+            except: print("failed to run locateCenter() on play_button")
+
+            if play_button != None:
+                try: mouseClick("play_button", play_button)
+                except: print("mouseClick failed [location= " + str(play_button) + "]")
+
     
 sys.exit(0)
