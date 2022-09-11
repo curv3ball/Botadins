4a0448af159a7e561f7e595373fe7ec14afa6100
Trey <39141616+curv3ball@users.noreply.github.com>
2022-09-11 02:36:04 -0400

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-
-play_button = pyautogui.locateCenterOnScreen('images/ui/play_button.png', region=(65, 175, 500, 350), confidence = 0.8)
-
-if play_button != None:
-    print("yes")
-else:
-    print("no")
